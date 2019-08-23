b197cf5f51caa44d7eea89d634484b889b080303
memo162 <jgespinosa91@gmail.com>
2019-08-23 14:51:31 -0500

    feature/application-layer: add application layer

---
+++ PresidentsList_API/PresidentsList.Application.Interfaces/President/IPresidentApplication.cs
@@ -0,0 +1,14 @@
+﻿using PresidentsList.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PresidentsList.Application.Interfaces
+{
+    public interface IPresidentApplication
+    {
+        List<President> Get();
+
+        List<President> Get(string orderBy);
+    }
+}
---
+++ PresidentsList_API/PresidentsList.Applications.Services/President/PresidentApplication.cs
@@ -0,0 +1,21 @@
+﻿using PresidentsList.Application.Interfaces;
+using PresidentsList.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PresidentsList.Applications.Services.PresidentApplication
+{
+    public class PresidentApplication : IPresidentApplication
+    {
+        public List<President> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<President> Get(string orderBy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
