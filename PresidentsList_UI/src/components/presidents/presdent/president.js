a990a37f2a038cb2f247925f2377650493e99904
memo162 <jgespinosa91@gmail.com>
2019-08-25 14:09:00 -0500

    feature/UI: add multiple presidents map

--- PresidentsList_UI/src/components/presidents/presdent/president.js
+++ PresidentsList_UI/src/components/presidents/presdent/president.js
@@ -6,39 +6,44 @@ export default class PresidentComponent extends React.Component {
     render() {
         return (
             <>
-                <div className="card mx-auto">
+                <div className="card mx-auto mb-2">
                     <div className="card-body p-2 pl-4">
                         <div className="row">
                             <div className="col-md-2 col-2 pl-1 pl-md-2 pl-lg-3">
-                                <a href="#" className="mx-auto">
-                                    <img alt="PRESIDENT NAME" className="img-fluid rounded-circle president-image"
+                                <a href={this.props.data.urlMoreInfo} 
+                                className="mx-auto">
+                                    <img alt="PRESIDENT NAME" 
+                                    className="img-fluid rounded-circle president-image"
                                     src="https://presidenstory.com/usimag/phot2/washington.jpg"/>
                                 </a>
                             </div>
                             <div className="col-md-9 col-10">
-                                    <a href="#" className="text-dark" alt="PRESIDENT NAME">
-                                        <h3 className="mb-0 name-small">George Washington</h3>
+                                    <a href={this.props.data.urlMoreInfo} 
+                                    className="text-dark" alt="PRESIDENT NAME">
+                                        <h3 className="mb-0 name-small">{
+                                            this.props.data.name}
+                                        </h3>
                                     </a>
                                 <div>
                                     <small>
                                         <div className="row">
                                             <div className="col-sm-4 col-lg-3 col-5 pr-0 pr-sm-auto">
                                                 <FaBirthdayCake className="mr-1"/>
-                                                <span>1732-2-22</span>
+                                                <span>{this.props.data.birthday.split('T')[0]}</span>
                                             </div>
                                             <div className="col-sm-8 col-lg-9 col-7 px-0 px-sm-auto">
                                                 <FaMapMarkerAlt className="mr-1 text-danger"/>
-                                                <span>Westmoreland Co., Va.</span>
+                                                <span>{this.props.data.birthplace}</span>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="col-sm-4 col-lg-3 col-5 pr-0 pr-sm-auto">
                                                 <FaCross className="mr-1"/>
-                                                <span>1799-12-14</span>
+                                                <span>{this.props.data.deathDay.split('T')[0]}</span>
                                             </div>
                                             <div className="col-sm-8 col-lg-9 col-7 px-0 px-sm-auto">
                                                 <FaMapMarkerAlt className="mr-1 text-danger"/>
-                                                <span>Mount Vernon, Va.</span>
+                                                <span>{this.props.data.deathPlace}</span>
                                             </div>
                                         </div>
                                     </small>
--- PresidentsList_UI/src/components/presidents/presidents.js
+++ PresidentsList_UI/src/components/presidents/presidents.js
@@ -4,6 +4,37 @@ import PresidentComponent from './presdent/president';
 import whiteHouseLogo from '../../assets/images/white_house_logo.png';
 
 export default class PresidentsComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.presidents = [
+            {
+                name: "George Washington",
+                birthday: "1732-02-22T00:00:00",
+                birthplace: "Westmoreland Co., Va.",
+                deathDay: "1799-12-14T00:00:00",
+                deathPlace: "Mount Vernon, Va.",
+                urlMoreInfo: "https://www.google.com",
+                urlPlaceInfo: "https://www.avianca.com",
+                id: 0,
+                idCreateUser: 0,
+                createDate: null
+            },
+            {
+                name: "Juan Espinosa",
+                birthday: "1732-02-22T00:00:00",
+                birthplace: "Westmoreland Co., Va.",
+                deathDay: "1799-12-14T00:00:00",
+                deathPlace: "Mount Vernon, Va.",
+                urlMoreInfo: "https://www.google.com",
+                urlPlaceInfo: "https://www.avianca.com",
+                id: 0,
+                idCreateUser: 0,
+                createDate: null
+            }
+        ];
+    }
+    
     render() {
         return (
             <>
@@ -22,11 +53,15 @@ export default class PresidentsComponent extends React.Component {
                         <div className="row text-white text-center">
                             <h1 className="col-md-12 pt-0 title">PRESIDENTS LIST</h1>
                         </div>
-                        <div className="row justify-content-md-center">
-                            <div className="col-md-10 col-lg-7">
-                                <PresidentComponent></PresidentComponent>
-                            </div>
-                        </div>
+                        {this.presidents.map(president => {
+                            return (
+                                <div className="row justify-content-md-center">
+                                    <div className="col-md-10 col-lg-7">
+                                        <PresidentComponent data={president}></PresidentComponent>
+                                    </div>
+                                </div>
+                            );
+                        })}
                         <br/>
                     </div>                
                 </div>
